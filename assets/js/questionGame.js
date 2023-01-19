04afd41fb99c3b65c0d14f0018a4067d03d9b038
vhivestate <garcia.valeria001@gmail.com>
2023-01-18 19:23:49 -0700

    finished

--- assets/js/questionGame.js
+++
@@ -1,8 +0,0 @@
-// this creates a function named "fight"
-
-function game() {
-    window.alert("your phone has been hacked LOL");
-    window.alert("however, I did have a question for you")
-  }
-
-  game()
